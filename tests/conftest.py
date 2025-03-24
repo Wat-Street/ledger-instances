5d1885bfc0b908c2683b22dc000ad1d39f8142c7
emmaashi <emmashi2006@gmail.com>
2025-03-24 15:41:38 -0400

    mock db connection

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,14 @@
 import pytest
-
 from tests.fixtures.fixture_artifact import sample_fixture
+from unittest.mock import patch, MagicMock
+
+@pytest.fixture
+def mock_db_connection():
+    with patch('utils.db_config.get_db_connection') as mock_get_db_connection:
+        mock_conn = MagicMock()
+        mock_get_db_connection.return_value.__enter__.return_value = mock_conn
+        yield mock_conn
+
 
 
 
