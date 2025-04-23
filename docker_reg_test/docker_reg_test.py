9a52965f93b5d88b7d7027bdfd62f50df1dac3da
reebxu459 <rebbi.xu123@gmail.com>
2025-04-22 23:25:46 -0400

    code
    Co-authored-by: Krish Chopra <krish.chopra@uwaterloo.ca>

---
+++ docker_reg_test/docker_reg_test.py
@@ -0,0 +1,47 @@
+import docker
+from utils.docker_utils import get_docker_client
+
+
+IP = "192.159.180.140"
+PORT = "5000"
+
+registry = f"{IP}:{PORT}"
+
+
+def tag_and_push_image():
+    """
+    Tags a Docker image with a new name/tag and pushes it to a registry.
+    """
+    image_name = "alpine:latest"
+    target_image = f"{registry}/my-alpine:latest"
+    try:
+        client = get_docker_client()
+        
+        # Tag the image
+        image = client.images.get(image_name)
+        image.tag(target_image)
+        print(f"Tagged {image_name} as {target_image}")
+        
+        # Push the image
+        response = client.images.push(target_image)
+        print(f"Pushed image to {target_image}")
+        
+        return response
+    except Exception as e:
+        raise RuntimeError(f"Error tagging or pushing Docker image: {e}")
+
+
+def pull_image():
+    client = get_docker_client()
+    image_to_pull = f"{registry}/my_alpine:latest"
+    image = client.images.pull(image_to_pull)
+    print(f"Pulled image: {image.tags}")
+
+
+if __name__ == "__main__":
+    print('Log: Tagging and Pushing Image')
+    tag_and_push_image()
+
+    print('Log: Pulling Image')
+    pull_image()
+
