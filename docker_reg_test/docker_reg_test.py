96f42b2f0af17dd71e42bd209a8445ea75b46ac9
Krish Chopra <krish.chopra23@gmail.com>
2025-05-08 23:18:35 -0400

    fixed docker test

--- docker_reg_test/docker_reg_test.py
+++ docker_reg_test/docker_reg_test.py
@@ -1,11 +1,11 @@
 import sys
 import docker
 
-sys.path.append('..')
+sys.path.append("..")
 from utils.docker_utils import get_docker_client
 
 
-IP = "192.159.180.140"
+IP = "localhost"
 PORT = "5000"
 
 registry = f"{IP}:{PORT}"
@@ -22,16 +22,16 @@ def tag_and_push_image():
 
         # Ensure image is pulled
         client.images.pull(image_name)
-        
+
         # Tag the image
         image = client.images.get(image_name)
         image.tag(target_image)
         print(f"Tagged {image_name} as {target_image}")
-        
+
         # Push the image
         response = client.images.push(target_image)
         print(f"Pushed image to {target_image}")
-        
+
         return response
     except Exception as e:
         raise RuntimeError(f"Error tagging or pushing Docker image: {e}")
@@ -39,15 +39,14 @@ def tag_and_push_image():
 
 def pull_image():
     client = get_docker_client()
-    image_to_pull = f"{registry}/test_alpine:latest"
+    image_to_pull = f"{registry}/test-alpine:latest"
     image = client.images.pull(image_to_pull)
     print(f"Pulled image: {image.tags}")
 
 
 if __name__ == "__main__":
-    print('Log: Tagging and Pushing Image')
+    print("Log: Tagging and Pushing Image")
     tag_and_push_image()
 
-    print('Log: Pulling Image')
+    print("Log: Pulling Image")
     pull_image()
-
